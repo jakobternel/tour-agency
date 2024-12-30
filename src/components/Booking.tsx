90c918ca9b0cb442c40a6273bdbfcd60b4bcc2f1
Strayyyy <jakobternel@gmail.com>
2024-12-30 11:29:53 +1100

    refactored booking itinerary section into separate component

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 
-import getUnicodeFlagIcon from "country-flag-icons/unicode";
-import { searchAirports } from "../utils/airportSearch";
+import Itinerary_Booking from "./booking/Itinerary_Booking";
 
 const Booking: React.FC<{
     itineraryContent: {
@@ -31,39 +30,15 @@ const Booking: React.FC<{
     };
 }> = ({ itineraryContent, bookingContent }) => {
     const [departureDate, setDepartureDate] = useState<Date | null>();
-    const [searchInput, setSearchInput] = useState<string>("");
-    const [searchActive, setSearchActive] = useState<boolean>(false);
-    const [fuzzySearchResults, setFuzzySearchResults] = useState<
-        { code: string; city: string; country: string }[]
-    >([]);
     const [flightSurcharge, setFlightSurcharge] = useState<number>(0);
     const [roomSelection, setRoomSelection] = useState<string>(
         String(bookingContent.defaultHotel)
     );
     const [roomSurcharge, setRoomSurcharge] = useState<number>(0);
-    const [tourLength] = useState<number>(
-        Object.values(itineraryContent).length
-    );
     const [optionalActivities, setOptionalActivities] = useState<number[]>([]);
     const [activitiesSurcharge, setActivitiesSurcharge] = useState<number>(0);
 
-    useEffect(() => {
-        const getFuzzySearchResults = async () => {
-            const results = await searchAirports(searchInput);
-
-            const airports = results.map((airport) => {
-                return {
-                    code: airport.item.iata_code,
-                    city: airport.item.municipality,
-                    country: airport.item.iso_country,
-                };
-            });
-
-            setFuzzySearchResults(airports);
-        };
-
-        getFuzzySearchResults();
-    }, [searchInput]);
+    const tourLength = Object.values(itineraryContent).length;
 
     useEffect(() => {
         setRoomSurcharge(
@@ -81,7 +56,7 @@ const Booking: React.FC<{
         });
 
         setActivitiesSurcharge(activitiesTotal);
-    });
+    }, [optionalActivities]);
 
     const formatDollarAmount = (amount: number) => {
         return Intl.NumberFormat("en-US", {
@@ -106,177 +81,14 @@ const Booking: React.FC<{
                         <div className="flex-grow bg-gray-300 h-1 rounded-full"></div>
                         <div className="text-gray-300">Payment</div>
                     </div>
-                    <div>
-                        <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex flex-row gap-1 items-center relative">
-                            <p className="w-1/2">
-                                Step 1 - Select Departure Airport
-                            </p>
-                            <div className="relative w-1/2">
-                                <input
-                                    type="text"
-                                    className="w-full border-2 border-primary p-1 focus:outline-none overflow-ellipsis pr-6"
-                                    onFocus={() => setSearchActive(true)}
-                                    onBlur={() => {
-                                        setTimeout(
-                                            () => setSearchActive(false),
-                                            150
-                                        );
-                                    }}
-                                    onChange={(e) => {
-                                        setSearchInput(e.target.value);
-                                    }}
-                                    value={searchInput}
-                                />
-                                {searchActive &&
-                                    fuzzySearchResults.length !== 0 && (
-                                        <div className="bg-red-50 w-full absolute z-10 border-2 border-primary border-t-0 flex flex-col">
-                                            {fuzzySearchResults.map(
-                                                (searchResult, index) => {
-                                                    return (
-                                                        <span
-                                                            key={index}
-                                                            onClick={() => {
-                                                                setSearchInput(
-                                                                    `${searchResult.code} - ${searchResult.city}, ${searchResult.country}`
-                                                                );
-                                                            }}
-                                                            className="bg-red-50 hover:bg-red-100 p-1 font-montserrat text-sm border-red-200 cursor-pointer [&:not(:last-child)]:border-b-2 flex flex-row justify-between"
-                                                        >
-                                                            <p>
-                                                                {`${searchResult.code} - ${searchResult.city}, ${searchResult.country}`}
-                                                            </p>
-                                                            <p>
-                                                                {getUnicodeFlagIcon(
-                                                                    searchResult.country
-                                                                )}
-                                                            </p>
-                                                        </span>
-                                                    );
-                                                }
-                                            )}
-                                        </div>
-                                    )}
-                                <i className="fi fi-br-search absolute top-1/2 -translate-y-1/2 right-2 text-primary"></i>
-                            </div>
-                        </div>
-                        <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex flex-row gap-1 items-center relative">
-                            <p className="w-1/2">
-                                Step 2 - Select Arrival Date
-                            </p>
-                            <input
-                                type="date"
-                                className="w-1/2 border-2 border-primary p-1 focus:outline-none"
-                                onChange={(e) => {
-                                    setDepartureDate(new Date(e.target.value));
-                                }}
-                                min={
-                                    new Date(
-                                        Date.now() + 7 * 24 * 60 * 60 * 1000
-                                    )
-                                        .toISOString()
-                                        .split("T")[0]
-                                }
-                                max={
-                                    new Date(
-                                        new Date().setFullYear(
-                                            new Date().getFullYear() + 1
-                                        )
-                                    )
-                                        .toISOString()
-                                        .split("T")[0]
-                                }
-                            />
-                            <i className="fi fi-br-calendar-day absolute right-5 text-primary"></i>
-                        </div>
-
-                        <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex flex-row gap-1 items-center relative">
-                            <p className="w-1/2">
-                                Step 3 - Select Accomodation Type
-                            </p>
-                            <select
-                                className="w-1/2 border-2 border-primary p-1 focus:outline-none"
-                                onChange={(e) => {
-                                    setRoomSelection(e.target.value);
-                                }}
-                                value={roomSelection}
-                            >
-                                {Object.keys(bookingContent.hotelContent).map(
-                                    (key) => {
-                                        return (
-                                            <option key={key} value={key}>
-                                                {
-                                                    bookingContent.hotelContent[
-                                                        key
-                                                    ].name
-                                                }
-                                                : +
-                                                {formatDollarAmount(
-                                                    bookingContent.hotelContent[
-                                                        key
-                                                    ].dailyAdditionalPrice
-                                                )}
-                                                /night
-                                            </option>
-                                        );
-                                    }
-                                )}
-                            </select>
-                            <i className="fi fi-br-angle-down absolute right-5 text-primary"></i>
-                        </div>
-
-                        <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex gap-1 flex-col">
-                            <p className="w-1/2">
-                                Step 4 - Select Additional Activities
-                            </p>
-                            <div className="flex flex-wrap mt-2 gap-3">
-                                {bookingContent.optionalActivities.map(
-                                    (activity, index) => {
-                                        return (
-                                            <p
-                                                className={`border-2 rounded-full border-primary py-1 px-3 text-sm cursor-pointer w-min text-nowrap transition-all ${
-                                                    optionalActivities.includes(
-                                                        index
-                                                    )
-                                                        ? "bg-red-100"
-                                                        : ""
-                                                }`}
-                                                onClick={() => {
-                                                    let newActivitiesArray: number[] =
-                                                        [];
-                                                    if (
-                                                        optionalActivities.includes(
-                                                            index
-                                                        )
-                                                    ) {
-                                                        newActivitiesArray =
-                                                            optionalActivities.filter(
-                                                                (item) =>
-                                                                    item !==
-                                                                    index
-                                                            );
-                                                    } else {
-                                                        newActivitiesArray = [
-                                                            ...optionalActivities,
-                                                            index,
-                                                        ];
-                                                    }
-
-                                                    setOptionalActivities(
-                                                        newActivitiesArray
-                                                    );
-                                                }}
-                                            >
-                                                <span className="font-bold">
-                                                    ${activity.cost}
-                                                </span>{" "}
-                                                {activity.name}
-                                            </p>
-                                        );
-                                    }
-                                )}
-                            </div>
-                        </div>
-                    </div>
+                    <Itinerary_Booking
+                        bookingContent={bookingContent}
+                        optionalActivities={optionalActivities}
+                        roomSelection={roomSelection}
+                        setDepartureDate={setDepartureDate}
+                        setRoomSelection={setRoomSelection}
+                        setOptionalActivities={setOptionalActivities}
+                    />
                     <div className="flex justify-end gap-6 items-center pr-6 mt-3">
                         <p className="text-gray-500 hover:text-primary transition-all hover:underline cursor-pointer">
                             Previous
---
+++ src/components/booking/Itinerary_Booking.tsx
@@ -0,0 +1,202 @@
+import { useEffect, useState } from "react";
+import { searchAirports } from "../../utils/airportSearch";
+
+import getUnicodeFlagIcon from "country-flag-icons/unicode";
+
+const Itinerary_Booking: React.FC<{
+    bookingContent: {
+        basePrice: number;
+        defaultHotel: number;
+        hotelContent: {
+            [key: string]: {
+                name: string;
+                dailyAdditionalPrice: number;
+            };
+        };
+        optionalActivities: {
+            name: string;
+            cost: number;
+        }[];
+    };
+    optionalActivities: number[];
+    roomSelection: string;
+    setDepartureDate: React.Dispatch<
+        React.SetStateAction<Date | null | undefined>
+    >;
+    setRoomSelection: React.Dispatch<React.SetStateAction<string>>;
+    setOptionalActivities: React.Dispatch<React.SetStateAction<number[]>>;
+}> = ({
+    bookingContent,
+    optionalActivities,
+    roomSelection,
+    setDepartureDate,
+    setRoomSelection,
+    setOptionalActivities,
+}) => {
+    const [searchInput, setSearchInput] = useState<string>("");
+    const [searchActive, setSearchActive] = useState<boolean>(false);
+    const [fuzzySearchResults, setFuzzySearchResults] = useState<
+        { code: string; city: string; country: string }[]
+    >([]);
+
+    useEffect(() => {
+        const getFuzzySearchResults = async () => {
+            const results = await searchAirports(searchInput);
+
+            const airports = results.map((airport) => {
+                return {
+                    code: airport.item.iata_code,
+                    city: airport.item.municipality,
+                    country: airport.item.iso_country,
+                };
+            });
+
+            setFuzzySearchResults(airports);
+        };
+
+        getFuzzySearchResults();
+    }, [searchInput]);
+
+    return (
+        <div>
+            <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex flex-row gap-1 items-center relative">
+                <p className="w-1/2">Step 1 - Select Departure Airport</p>
+                <div className="relative w-1/2">
+                    <input
+                        type="text"
+                        className="w-full border-2 border-primary p-1 focus:outline-none overflow-ellipsis pr-6"
+                        onFocus={() => setSearchActive(true)}
+                        onBlur={() => {
+                            setTimeout(() => setSearchActive(false), 150);
+                        }}
+                        onChange={(e) => {
+                            setSearchInput(e.target.value);
+                        }}
+                        value={searchInput}
+                    />
+                    {searchActive && fuzzySearchResults.length !== 0 && (
+                        <div className="bg-red-50 w-full absolute z-10 border-2 border-primary border-t-0 flex flex-col">
+                            {fuzzySearchResults.map((searchResult, index) => {
+                                return (
+                                    <span
+                                        key={index}
+                                        onClick={() => {
+                                            setSearchInput(
+                                                `${searchResult.code} - ${searchResult.city}, ${searchResult.country}`
+                                            );
+                                        }}
+                                        className="bg-red-50 hover:bg-red-100 p-1 font-montserrat text-sm border-red-200 cursor-pointer [&:not(:last-child)]:border-b-2 flex flex-row justify-between"
+                                    >
+                                        <p>
+                                            {`${searchResult.code} - ${searchResult.city}, ${searchResult.country}`}
+                                        </p>
+                                        <p>
+                                            {getUnicodeFlagIcon(
+                                                searchResult.country
+                                            )}
+                                        </p>
+                                    </span>
+                                );
+                            })}
+                        </div>
+                    )}
+                    <i className="fi fi-br-search absolute top-1/2 -translate-y-1/2 right-2 text-primary"></i>
+                </div>
+            </div>
+            <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex flex-row gap-1 items-center relative">
+                <p className="w-1/2">Step 2 - Select Arrival Date</p>
+                <input
+                    type="date"
+                    className="w-1/2 border-2 border-primary p-1 focus:outline-none"
+                    onChange={(e) => {
+                        setDepartureDate(new Date(e.target.value));
+                    }}
+                    min={
+                        new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)
+                            .toISOString()
+                            .split("T")[0]
+                    }
+                    max={
+                        new Date(
+                            new Date().setFullYear(new Date().getFullYear() + 1)
+                        )
+                            .toISOString()
+                            .split("T")[0]
+                    }
+                />
+                <i className="fi fi-br-calendar-day absolute right-5 text-primary"></i>
+            </div>
+
+            <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex flex-row gap-1 items-center relative">
+                <p className="w-1/2">Step 3 - Select Accomodation Type</p>
+                <select
+                    className="w-1/2 border-2 border-primary p-1 focus:outline-none"
+                    onChange={(e) => {
+                        setRoomSelection(e.target.value);
+                    }}
+                    value={roomSelection}
+                >
+                    {Object.keys(bookingContent.hotelContent).map((key) => {
+                        return (
+                            <option key={key} value={key}>
+                                {bookingContent.hotelContent[key].name}: +$
+                                {
+                                    bookingContent.hotelContent[key]
+                                        .dailyAdditionalPrice
+                                }
+                                /night
+                            </option>
+                        );
+                    })}
+                </select>
+                <i className="fi fi-br-angle-down absolute right-5 text-primary"></i>
+            </div>
+
+            <div className="border-b-gray-200 border-b-2 py-3 pr-3 flex gap-1 flex-col">
+                <p className="w-1/2">Step 4 - Select Additional Activities</p>
+                <div className="flex flex-wrap mt-2 gap-3">
+                    {bookingContent.optionalActivities.map(
+                        (activity, index) => {
+                            return (
+                                <p
+                                    className={`border-2 rounded-full border-primary py-1 px-3 text-sm cursor-pointer w-min text-nowrap transition-all ${
+                                        optionalActivities.includes(index)
+                                            ? "bg-red-100"
+                                            : ""
+                                    }`}
+                                    onClick={() => {
+                                        let newActivitiesArray: number[] = [];
+                                        if (
+                                            optionalActivities.includes(index)
+                                        ) {
+                                            newActivitiesArray =
+                                                optionalActivities.filter(
+                                                    (item) => item !== index
+                                                );
+                                        } else {
+                                            newActivitiesArray = [
+                                                ...optionalActivities,
+                                                index,
+                                            ];
+                                        }
+
+                                        setOptionalActivities(
+                                            newActivitiesArray
+                                        );
+                                    }}
+                                >
+                                    <span className="font-bold">
+                                        ${activity.cost}
+                                    </span>{" "}
+                                    {activity.name}
+                                </p>
+                            );
+                        }
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Itinerary_Booking;
