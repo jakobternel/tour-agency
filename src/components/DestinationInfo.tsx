d2ae0f98df91cebc371f48e1ae053d2235365052
Strayyyy <jakobternel@gmail.com>
2025-04-02 23:28:48 +1100

    removed destination images from destination info section on mobile

--- src/components/DestinationInfo.tsx
+++ src/components/DestinationInfo.tsx
@@ -6,9 +6,9 @@ const DestinationLayout: React.FC<{
     bookingRef: React.MutableRefObject<HTMLDivElement | null>;
 }> = ({ data, scrollToRef, bookingRef }) => {
     return (
-        <div className="h-screen w-full bg-primary px-20 py-20">
+        <div className="md:h-screen w-full bg-primary p-6 md:px-20 md:py-20">
             <div className="h-full w-full flex flex-row">
-                <div className="h-full w-2/5 flex flex-col justify-between flex-wrap content-between">
+                <div className="hidden h-full w-2/5 md:flex flex-col justify-between flex-wrap content-between">
                     {Object.values(data.destinationImages).map(
                         (
                             image: { size: string; fileSrc: string },
@@ -37,7 +37,7 @@ const DestinationLayout: React.FC<{
                         }
                     )}
                 </div>
-                <div className="bg-white h-full w-3/5 ml-6 rounded-xl p-10 shadow-lg flex gap-6 flex-col">
+                <div className="bg-white md:h-full md:w-3/5 md:ml-6 rounded-xl p-8 md:p-10 shadow-lg flex gap-6 flex-col">
                     <p className="font-lobster w-min text-3xl whitespace-nowrap -rotate-6 origin-center font-extralight text-primary">
                         {`${data.city}, ${data.country}`}
                     </p>
@@ -45,11 +45,15 @@ const DestinationLayout: React.FC<{
                         {data.tagline}
                     </p>
                     {data.description.map((text, index) => {
-                        return <p key={index}>{text}</p>;
+                        return (
+                            <p className="text-sm md:text-md" key={index}>
+                                {text}
+                            </p>
+                        );
                     })}
-                    <span className="flex-grow"></span>
+                    <span className="hidden md:block md:flex-grow"></span>
                     <button
-                        className="border-primary border-2 w-min whitespace-nowrap py-2 px-5 rounded-xl hover:bg-red-100 font-semibold font-montserrat transition-all"
+                        className="border-primary border-2 w-min whitespace-nowrap py-2 px-5 rounded-xl hover:bg-red-100 font-semibold font-montserrat transition-all text-sm md:text-md"
                         onClick={() => scrollToRef(bookingRef)}
                     >
                         Book Now!
